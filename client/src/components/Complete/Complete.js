2895549d4036ea84112eafa7cbe92f963ae31936
Okkio <60454980+Okkio@users.noreply.github.com>
2020-10-17 01:22:13 +0100

    HomePage + style change

--- client/src/components/Complete/Complete.js
+++ client/src/components/Complete/Complete.js
@@ -1,10 +1,12 @@
 import React, {useState, useEffect} from 'react';
+import classes from '../../CSS/Complete.module.css'
 
 const Complete = () =>{
     return(
-        <>
-            <label>COMPLETE!</label>
-        </>
+        <div className={classes.completeContainer}>
+            <label className={classes.Complete}>Puzzle Solved!</label>
+            {/* <p>Check your score in the Rankings page</p> */}
+        </div>
     )
 }
 export default Complete;
